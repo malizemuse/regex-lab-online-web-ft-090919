9137857e3bf7522df9bf9cccf3b478abf62940aa
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-16 01:33:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -33,7 +33,13 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  
+    if text.match(/^[A-Z]/) && text.match(/[\.]$/)
+    true
+  else 
+    false
+  end
+  
 end
 
 
